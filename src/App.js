c0791910ac06f86aea03e13eb477315c9d484eac
lenghia148 <ledinhnghia.qb@gmail.com>
2022-07-19 00:31:12 +0700

    simple

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Following from './pages/Following';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        
+        
+      </div>
+    </Router>
+  );
+}
+
+export default App;
